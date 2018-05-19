c1f7507101daac4a02e422600ec85944f18ac943
MrGauss <github@cmska.org>
2018-05-19 12:01:50 +0300

    autoupdate 2018-05-19-12:01:50

---
+++ cache/cache-posts-8289f621f937af2517f0cebbe4a0fd07.cmska.org.php
@@ -0,0 +1,9 @@
+<?php if( !defined('GAUSS_CMS') ){ echo basename(__FILE__); exit; }
+ /* CACHE CREATED: 1526693169.4577 (2018-05-19 04:26:09) */
+return array (
+  'count' => 0,
+  'rows' => 
+  array (
+  ),
+); 
+?>
---
+++ cache/cache-posts-9a41a8c43691affdc61dfab76306f73c.cmska.org.php
@@ -0,0 +1,9 @@
+<?php if( !defined('GAUSS_CMS') ){ echo basename(__FILE__); exit; }
+ /* CACHE CREATED: 1526687306.3823 (2018-05-19 02:48:26) */
+return array (
+  'count' => 0,
+  'rows' => 
+  array (
+  ),
+); 
+?>
