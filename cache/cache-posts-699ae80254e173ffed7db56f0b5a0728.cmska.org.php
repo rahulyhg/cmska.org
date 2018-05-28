ae51089cb3442aa4e8d94597461daf57cd51d16e
MrGauss <github@cmska.org>
2018-05-29 01:02:05 +0300

    autoupdate 2018-05-29-01:02:05

---
+++ cache/cache-posts-699ae80254e173ffed7db56f0b5a0728.cmska.org.php
@@ -0,0 +1,9 @@
+<?php if( !defined('GAUSS_CMS') ){ echo basename(__FILE__); exit; }
+ /* CACHE CREATED: 1527477738.1722 (2018-05-28 06:22:18) */
+return array (
+  'count' => 0,
+  'rows' => 
+  array (
+  ),
+); 
+?>
