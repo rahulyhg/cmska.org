33f8736a0c3905a6ddf3b96baa6c56405202e8bb
MrGauss <github@cmska.org>
2018-06-24 01:04:13 +0300

    autoupdate 2018-06-24-01:04:13

---
+++ cache/cache-posts-b428cf282994c7dc6fa67c791d06bc11.cmska.org.php
@@ -0,0 +1,41 @@
+<?php if( !defined('GAUSS_CMS') ){ echo basename(__FILE__); exit; }
+ /* CACHE CREATED: 1529791370.6918 (2018-06-24 01:02:50) */
+return array (
+  'count' => 1,
+  'rows' => 
+  array (
+    2 => 
+    array (
+      'post' => 
+      array (
+        'id' => '2',
+        'title' => 'define',
+        'alt_title' => 'define',
+        'descr' => '',
+        'short_post' => 'В даній публікації я познайомлю Вас з деякими парадигмами, константами CMS яку ми пишемо.',
+        'author_id' => '1',
+        'created_time' => '2017-02-12 22:03:08',
+        'keywords' => '',
+        'posted' => '1',
+        'fixed' => '0',
+        'static' => '0',
+        'full_post' => '<code class="bb_code">define&lpar; &apos;open&lowbar;source&apos;&comma; true &rpar;&semi;</code>&#10;<p class=\\"bb_p\\">Дана CMS є розробкою з відкритим вихідним кодом, тобто кожен бажаючий може використовувати її в власних цілях. Група розробників сайту "cmska.org" залишає за собою право розробки та супроводу даного програмного продукту.</p>&#10;<br>&#10;<code class="bb_code">define&lpar; &apos;language&apos;&comma; &apos;&Ucy;&kcy;&rcy;&acy;&yicy;&ncy;&scy;&softcy;&kcy;&acy;&apos; &rpar;&semi;</code>&#10;<p class=\\"bb_p\\">Розробники "cmska.org" є громадянами України, а тому на даному ресурсі та в розробці використовується лише Українська мова.</p>&#10;<br>&#10;<code class="bb_code">define&lpar; &apos;security&lowbar;level&apos;&comma; &apos;&Ncy;&acy;&jcy;&vcy;&icy;&shchcy;&icy;&jcy;&apos; &rpar;&semi;</code>&#10;<p class=\\"bb_p\\">В своїй діяльності розробники керуються принципом максимальної захищеності як користувачів так і сайту. З метою здійснення цього принципу розробники зобов\'язуються в межах своєї компетенції використовувати всі методи підвищення рівня захисту.</p>',
+      ),
+      'categ' => 
+      array (
+        'id' => '1',
+        'altname' => 'blog',
+        'name' => 'Блог розробників',
+      ),
+      'usr' => 
+      array (
+        'login' => 'admin',
+        'email' => 'admin@cmska.org',
+      ),
+      'tags' => 
+      array (
+      ),
+    ),
+  ),
+); 
+?>
